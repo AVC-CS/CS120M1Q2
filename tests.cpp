c32c52d4a0eae00545ed6d44be7777ec49ecce64
Kyu Lee <kwooler@gmail.com>
2023-03-15 11:35:22 -0700

    Q3

--- tests.cpp
+++ tests.cpp
@@ -8,39 +8,42 @@ TEST_CASE("Ex1 getRdnum()", "[example]")
 	int result = 1;
 	int tmp, fault;
 
-	for(int i=0;i<10; i++)
+	srand(time(0));
+	for (int i = 0; i < 20; i++)
 	{
 		tmp = getRdnum();
-		cout << tmp << endl;
+		cout << tmp << "\t";
 		if ((tmp <= 0) || (tmp > 10))
 		{
 			fault = tmp;
-			result  = 0;
+			result = 0;
 		}
 	}
-	INFO("Your random number is " << fault );
-	INFO("Your random number shoub be between 1 and 10" );
-	REQUIRE(result == 1 );
+	cout << endl;
+	INFO("Your random number is " << fault);
+	INFO("Your random number shoub be between 1 and 10");
+	REQUIRE(result == 1);
 }
 // tests for exercise 2
 TEST_CASE("Ex2 getRdnum(21, 25)", "[example]")
 {
 	int result = 1;
 	int tmp, fault;
-	for(int i=0;i<10; i++)
+	srand(time(0));
+	for (int i = 0; i < 20; i++)
 	{
 
-		tmp = getRdnum(21, 25);
-		cout << tmp << endl;
+		tmp = getRdnum(20, 25);
+		cout << tmp << "\t";
 		if ((tmp < 21) || (tmp > 25))
 		{
 			fault = tmp;
-			result  = 0;
+			result = 0;
 		}
-	
 	}
-	INFO("Your random number is " << fault );
-	INFO("Your random number shoub be between 21 and 25" );
+	cout << endl;
+	INFO("Your random number is " << fault);
+	INFO("Your random number shoub be between 21 and 25");
 	REQUIRE(result == 1);
 }
 // tests for exercise 3
@@ -48,17 +51,19 @@ TEST_CASE("Ex3: getRdnum(5)", "[example]")
 {
 	int result = 1;
 	int tmp, fault;
-	for(int i=0;i<10; i++)
+	srand(time(0));
+	for (int i = 0; i < 20; i++)
 	{
 		tmp = getRdnum(5);
-		cout << tmp << endl;
+		cout << tmp << "\t";
 		if ((tmp <= 0) || (tmp > 5))
 		{
 			fault = tmp;
-			result  = 0;
+			result = 0;
 		}
 	}
-	INFO("Your random number is " << fault );
-	INFO("Your random number shoub be between 0 and 4" );
+	cout << endl;
+	INFO("Your random number is " << fault);
+	INFO("Your random number shoub be between 1 and 5");
 	REQUIRE(result == 1);
 }
